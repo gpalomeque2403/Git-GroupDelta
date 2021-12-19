7c6a62b14b39b51206d9d5f0d31878683f99d25d
jvonlandwust <jvonlandwust@doc.ulasalle.edu.bo>
2021-12-18 21:22:29 -0400

    Se agregaron mensajes a la salida de la terminal

--- Shell-GroupDelta.sh
+++ Shell-GroupDelta.sh
@@ -10,7 +10,11 @@ cd Shell-GroupDelta
 docker build -t groupdelta .
 docker run -t -d --name groupdeltad groupdelta
 docker ps -a
+echo "********************************** 2A **********************************"
 docker exec -it groupdeltad python3 /Git-GroupDelta/2a-meraki.py
+echo "********************************** 2B **********************************"
 docker exec -it groupdeltad python3 /Git-GroupDelta/2b-dnacenter.py
+echo "********************************** 2C **********************************"
 docker exec -it groupdeltad python3 /Git-GroupDelta/2c-csr100v.py
-docker exec -it groupdeltad python3 /Git-GroupDelta/2d-webex.py
+echo "********************************** 2D **********************************"
+#docker exec -it groupdeltad python3 /Git-GroupDelta/2d-webex.py
